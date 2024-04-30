f98dc391a74ffd4f80bea1b7f810fc912bd86f13
C4102112 <114786511+C4102112@users.noreply.github.com>
2024-04-30 20:01:34 -0300

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,92 @@
+<template>
+<body>
+    <section>
+        <img src="..\assets\perfil.jpg" class="img-thumbnail" alt="...">
+    </section>
+    <header>
+        <h1>Caio Vinicius Costa Nascimento</h1>
+    </header>
+    <section>
+        <h2>Sobre Mim</h2>
+        <p>Olá, meu nome é Caio Vinicius. Eu sou estudante de T.I na Unimar. Eu tenho interesse em tudo que tem haver com Tecnologia.</p>
+    </section>
+    <section>
+        <h2>Educação</h2>
+        <p>Atualmente, estou cursando Analise e desenvolvimento de Sistemas na Unimar.</p>
+    </section>
+
+    <section>
+        <h2>Trabalho</h2>
+        <p>Atualmente, estou trabalhando como Técnico de Manutenção de Eletrônicos na Empresa Alpha Cell.</p>
+    </section>
+
+    <section>
+        <h2>Hobbies e Interesses</h2>
+        <ul class="text-center">
+            <li>Jogar Futebol</li>
+            <li>Jogar Video Game</li>
+            <li>Programar</li>
+        </ul>
+    </section>
+    <footer>
+        <p>Contato: caioc0415@gmail.com</p>
+    </footer>
+</body>
+</template>
+
+<style>
+    body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f9;
+    color: #333;
+}
+
+        .img-thumbnail {
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        border: 5px solid #0056b3;
+    }
+
+header, section, footer {
+    margin-bottom: 20px;
+    padding: 20px;
+    border-radius: 8px;
+    background-color: white;
+    box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 10px;
+}
+
+h1, h2 {
+    color: #0056b3;
+    display: flex;
+    justify-content: center; 
+    align-items: center; 
+    margin: 14px;
+    flex-direction: column;
+    gap: 10px;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    padding: 8px;
+    background-color: #e9ecef;
+    margin-bottom: 5px;
+    border-radius: 4px;
+}
+
+footer {
+    font-size: small;
+    text-align: center;
+    color: #666;
+}
+    
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<script setup>
+//import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, reactive, ref } from 'vue';
+
+let personagens = reactive(ref());
+let types = reactive(ref());
+
+onMounted(() => {
+  fetch("https://rickandmortyapi.com/api/character?limit=60&offset=0")
+  .then(response => response.json())
+  .then(response => {
+    personagens.value = response.results;
+    console.log(personagens);
+  })
+})
+
+</script>
+
+<template>
+  <div class="container">
+  <div class="row">
+          <div class="col-md-6" v-for="personagem in personagens" :key="personagem.id">
+                <div class="card mb-3" style="max-width: 700px;">
+                    <div class="row g-0 p-2 ">
+                        <div class="col-md-4">
+                            <img :src="personagem.image" class="card-img-top" height="100%" alt="...">
+                        </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{{ personagem.name }}</h5>
+        <p class="card-text">Status: {{ personagem.status }}</p>
+        <p class="card-text">Espécie: {{ personagem.species }}</p>
+        <p class="card-text">Gênero: {{ personagem.gender }}</p>
+        <p class="card-text">Episodios: {{ personagem.episode.length }}</p>
+        <p class="card-text"><small class="text-body-secondary">Localização atual: {{ personagem.location.name }}</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+  </template>
+
+<style>
+.container {
+  padding-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+  
